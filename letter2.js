7166129a4a1d4a87704fca40478727ea025aa552
Charles Prestia <cjprestia@gmail.com>
2016-10-03 16:55:40 -0400

    at least it's starting to work nowgit add -A

--- letter2.js
+++ letter2.js
@@ -7,18 +7,26 @@ var updateWord = require("./word2.js");
 var fs = require('fs');
 
 // randWord.randomWord[0].rand_word;
-var currentWord = randWord.randomWord.rand_word;
-var currentDefault = randWord.randomWord.rand_default;
-var currLetArray = [];
-var pushCount = 0;
+
+// var currentWord = randWord.randomWord.rand_word;
+// var currentDefault = randWord.randomWord.rand_default;
+// var currLetArray = [];
+
 //currLetArray = array of indecies for where the location of the user's
 //  letter within the current wor
+console.log("letter.js module.exports is connected.");
 
-module.exports = function(userInput) {
-	console.log("letter.js module.exports is connected.");
-	// this.userInput = userInput;
+module.exports = {
 
-	checked: function() {
+	checked: function(userInput) {
+		console.log("letter.js module.exports is connected.")
+
+		var currentWord = randWord.randomWord[0];
+		var currentDefault = randWord.randomWord[1];
+		var displayWordProg = currentDefault;
+		var currLetArray = [];
+		this.userInput = userInput;
+
 		for(var i = 0; i < currentWord.length; i++){
 			if(userInput == currentWord.charAt[i]) {
 				currLetArray.push(i);
@@ -26,10 +34,9 @@ module.exports = function(userInput) {
 		}
 		return currLetArray;
 	},
-	count: function() {
+	// count: function() {
 
-	}
-	};
+	// }
 };
 
 
--- word2.js
+++ word2.js
@@ -8,36 +8,33 @@ var fs = require('fs');
 
 
 // randWord.randomWord[0].rand_word;
-var currentWord = randWord.randomWord[0];
-var currentDefault = randWord.randomWord[1];
-var displayWordProg = currentDefault[];
+
+// var currentWord = randWord.randomWord[0];
+// var currentDefault = randWord.randomWord[1];
+// var displayWordProg = currentDefault;
+
 //Array of letter indeces to reveal in updated word
 // var displayLetArray = letterCheck.checkedLetter;
 
 module.exports = {
-	function(resultLetterCheck) {
-	console.log("word.js module.exports is connected.");
-	this.currLetArray = currLetArray;
+	updated: function(resultLetterCheck) {
+		console.log("word.js module.exports is connected.");
+		var currentWord = randWord.randomWord[0];
+		var currentDefault = randWord.randomWord[1];
+		var displayWordProg = currentDefault;
+		this.currLetArray = currLetArray;
 
-	return {
-		updated: function() {
-
-			for(var i = 0; i < displayLetArray.length; i++){
-	//for each number in the array, I want to replace the
-	// number at that index in the default string with the
-	// corresponding letter at that index in the word string
-				var letIndex =  displayLetArray[i];
-				var letFromWord = currentWord.charAt(letIndex);
-				var displayWordProg = setCharAt(displayWordProg, letIndex, letFromWord);
-
-
-			}
-			return displayWordProg;
+		for(var i = 0; i < displayLetArray.length; i++){
+//for each number in the array, I want to replace the
+// number at that index in the default string with the
+// corresponding letter at that index in the word string
+			var letIndex =  displayLetArray[i];
+			var letFromWord = currentWord.charAt(letIndex);
+			var displayWordProg = setCharAt(displayWordProg, letIndex, letFromWord);
 		}
-	};
-
+		return displayWordProg;
+	}
 };
-
 // function setCharAt(str, index, char) {
 // 	if(index > str.length-1) {
 // 		return str;
